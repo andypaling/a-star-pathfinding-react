d7ee193870322e2e0d7df89a57c673e1ee06c271
Andy Paling <andrew@loenhouse.com>
2021-01-20 11:30:48 +0000

    fixed gitignore issue

--- src/Components/Home/HowAStarWorks/MathFunctionFCharacter.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function MathFunctionFCharacter(props) {
-    return (
-         <strong><i>'<span>&#402;</span>'</i></strong>
-    );
-}
-
-export default MathFunctionFCharacter;
